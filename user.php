44649c3b89b43e38df837dc5acc7fa09b962ed66
Matt Warren <matt@warrencomputing.net>
2013-02-27 00:00:56 -0500

    added logic for inserting passwords into htpasswd file. untested

--- user.php
+++ user.php
@@ -2,6 +2,7 @@
 include 'config.php';
 
 $check_pw=$_POST['confirm'];
+$username = $_POST['uname'];
 
 if (isset($_POST['pass'])){
   $pass = isset($_POST['pass']) ? $_POST['pass'] : '';
@@ -10,6 +11,13 @@ if (isset($_POST['pass'])){
     showForm("Passwords do not match!");
     exit();
   } else {
+    $crpyt_pass = crypt($pass, base64_encode($pass));
+    $unpw_line = $username . ":" . $crypt_pass
+    if (file_exists($htpw_file)){
+      file_put_contents($htpw_file, $unpw_line, FILE_APPEND);
+    } else {
+      file_put_contents($htpw_file, $unpw_line);
+    }
     showForm("Match!");
     exit();
   }
