5f477da23ce952129093c788929d30c8d7fb7936
Matt Warren <matt@warrencomputing.net>
2013-03-03 22:45:12 -0500

    pretty sure I can call this one a beta release

--- user.php
+++ user.php
@@ -7,26 +7,52 @@ $username=$_POST['uname'];
 if (isset($_POST['pass'])){
   $pass = isset($_POST['pass']) ? $_POST['pass'] : '';
 
-  $passfile = file($htpw_file);
+  if (file_exists($htpw_file)){
+    $passfile = file($htpw_file);
+    $count = count($passfile);
 
-  if ($pass != $check_pw){
-    showForm("Passwords do not match!");
-    exit();
-  } else {
-    $crypt_pass = crypt($pass, base64_encode($pass));
-    $unpw_line = $username . ":" . $crypt_pass . PHP_EOL;
-    if (file_exists($htpw_file)){
-      if (in_array($unpw_line, $passfile)){
-        showForm ("User already exists!  You cannot overwrite it!");
-        exit();
-      } else {
-        file_put_contents($htpw_file, $unpw_line, FILE_APPEND);
+    foreach ($passfile as $line){
+      $split_line = split(":", $line);
+      $stored_un[] = $split_line[0];
+      $stored_pw[] = $split_line[1];
+    }
+
+    if ($pass != $check_pw){
+      showForm("Passwords do not match!");
+      exit();
+
+    } else {
+      $form_str = "Match!";
+      $crypt_pass = crypt($pass, base64_encode($pass));
+      $unpw_line = $username . ":" . $crypt_pass . PHP_EOL;
+
+        if (in_array($username, $stored_un)){
+          $form_str = "User already exists!  Changing password!";
+          $keys = array_keys($stored_un, $username);
+
+          foreach ($keys as $key){
+            $stored_pw[$key] = $crypt_pass;
+          }
+
+          for ($i = 0; $i < $count; $i++){
+            $curr_un = $stored_un[$i];
+            $curr_pw = $stored_pw[$i];
+            $to_write[] = $curr_un . ":" . $curr_pw;
+          }
+
+          file_put_contents($htpw_file, $to_write);
+
+        } else {
+          file_put_contents($htpw_file, $unpw_line, FILE_APPEND);
+        }
       }
+
     } else {
       file_put_contents($htpw_file, $unpw_line);
     }
-    showForm("Match!");
-    exit();
+
+  showForm($form_str);
+  exit();
   }
 } else {
   showForm("Please provide a password");
